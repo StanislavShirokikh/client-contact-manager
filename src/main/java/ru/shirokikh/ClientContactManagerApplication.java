d75aa17902cdde750bfb96ba1f68b5bc20b15357
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-11-27 19:48:49 +0700

    Initial commit

---
+++ src/main/java/ru/shirokikh/ClientContactManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.shirokikh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientContactManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shirokikh/ClientContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shirokikh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
